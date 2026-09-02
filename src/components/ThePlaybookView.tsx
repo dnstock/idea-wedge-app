e57fb87787d86eb6a405feb7b1a933f5c563f549
Dan Harcsztark <85728+dnstock@users.noreply.github.com>
2026-09-02 14:27:07 -0400

    Add button to view The Playbook in GDocs for commenting

--- src/components/ThePlaybookView.tsx
+++ src/components/ThePlaybookView.tsx
@@ -2,17 +2,29 @@ import { useRef } from 'react';
 import { useGoogleDoc } from '../hooks/useGoogleDoc';
 
 export function ThePlaybookView() {
-  const publishedUrl = import.meta.env.VITE_PLAYBOOK_GDOC_URL || '';
+  const publishedUrl = import.meta.env.VITE_PLAYBOOK_GDOC_PUB_URL;
+  const commentsUrl = import.meta.env.VITE_PLAYBOOK_GDOC_SHARE_URL;
+  if (!publishedUrl) {
+    return (
+      <section className="card section-stack playbook-view">
+        The Playbook URL is not set in the environment variables.
+      </section>
+    );
+  }
   const containerRef = useRef<HTMLDivElement | null>(null);
   const { loading, error } = useGoogleDoc({ publishedUrl, containerRef });
 
+  function onViewInGoogleDocs() {
+    window.open(commentsUrl, '_blank');
+  }
+
   return (
     <section className="card section-stack playbook-view">
-      {!publishedUrl && (
-        <div className="p-4 text-red-500 border border-red-200 rounded">
-          VITE_PLAYBOOK_GDOC_URL is not set in the environment variables.
-        </div>
-      ) || (
+      {commentsUrl && (
+        <button id="view-in-google-docs" className="button primary nobreak" onClick={onViewInGoogleDocs} type="button">
+          View &amp; Comment in Google Docs
+        </button>
+      )}
       <div className="gdoc-wrapper">
         {loading && (
           <div className="p-4 text-gray-500">Idea Wedge Playbook is loading...</div>
@@ -30,7 +42,6 @@ export function ThePlaybookView() {
           style={{ display: loading ? 'none' : 'block' }}
         />
       </div>
-      )}
     </section>
   );
 }
